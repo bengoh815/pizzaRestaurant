88fe59ba2f99401bb4c13d74552e9ff192f56e37
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2023-02-19 22:31:07 -0600

    Fixed bug
    Forgot to upload it yesterday

--- pizzarestaurant/components/PizzaCard.jsx
+++ pizzarestaurant/components/PizzaCard.jsx
@@ -4,7 +4,9 @@ import Image from 'next/image';
 export default function PizzaCard() {
     return (
         <div className={styles.container}>
-            <Image src='/img/pizza.png' alt="" width="500" height="500" />
+            <div className={styles.imageContainer}>
+                <Image src='/img/pizza.png' alt="" fill style={{objectFit:"contain"}} />
+            </div>
             <h1 className={styles.title}>SOME PIZZA</h1>
             <span className={styles.price}>$12.34</span>
             <p className={styles.description}>Lorem ipsum dolor sit amet</p>
