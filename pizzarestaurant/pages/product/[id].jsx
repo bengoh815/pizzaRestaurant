66f9559d4af73b43b7dc6a9b2a8a4e9be663eac8
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2023-02-23 23:29:46 -0600

    Added some cod
    need to find pizza size image

--- pizzarestaurant/pages/product/[id].jsx
+++ pizzarestaurant/pages/product/[id].jsx
@@ -24,6 +24,20 @@ export default function Product() {
                 <span className={styles.price}>${pizza.price[size]}</span>
                 <p className={styles.desc}>{pizza.desc}</p>
                 <h3 className={styles.choose}>Choose your size</h3>
+                <div className={styles.sizes}>
+                    <div className={styles.size}>
+                        <Image src="/img/size.png" alt="" fill/>
+                        <span className={styles.number}>Small</span>
+                    </div>
+                    <div className={styles.size}>
+                        <Image src="/img/size.png" alt="" fill/>
+                        <span className={styles.number}>Medium</span>
+                    </div>
+                    <div className={styles.size}>
+                        <Image src="/img/size.png" alt="" fill/>
+                        <span className={styles.number}>Large</span>
+                    </div>
+                </div>
             </div>
         </div>
     )
