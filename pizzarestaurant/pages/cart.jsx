e2a97d706c5d693e35db6dd1e6bb0d9c04aa47a7
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2023-02-27 21:58:23 -0600

    mysterious hydration error

--- pizzarestaurant/pages/cart.jsx
+++ pizzarestaurant/pages/cart.jsx
@@ -1,28 +1,58 @@
 import styles from "../styles/Cart.module.css"
+import Image from "next/image"
 
 export default function Cart() {
     return (
         <div className={styles.container}>
             <div className={styles.left}>
-            <table className={styles.table}>
-                <tr className={styles.tr}>
-                    <th>Product</th>
-                    <th>Name</th>
-                    <th>Extras</th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                    <th>Total</th>
-                </tr>
-                <tr>
-                    <td>
-                        <div className={styles.imgContainer}>
-                            <Image src="" alt="" fill styles={{objectFit:contain}}/>
-                        </div>
-                    </td>
-                </tr>
-            </table>
+                <table className={styles.table}>
+                    <tr className={styles.tr}>
+                        <th>Product</th>
+                        <th>Name</th>
+                        <th>Extras</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Total</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div className={styles.imgContainer}>
+                                <Image src="/img/pizza.png" alt="" fill styles={{objectFit:"contain"}}/>
+                            </div>
+                        </td>
+                        <td>
+                            <span className={styles.name}>CORALZO</span>
+                        </td>
+                        <td>
+                            <span className={styles.extras}>Double ingrediant, spicy sauce</span>
+                        </td>
+                        <td>
+                            <span className={styles.price}>$19.90</span>
+                        </td>
+                        <td>
+                            <span className={styles.quantity}>2</span>
+                        </td>
+                        <td>
+                            <span className={styles.total}>$39.80</span>
+                        </td>
+                    </tr>
+                </table>
             </div>
-            <div className={styles.right}></div>
+            <div className={styles.right}>
+                <div className={styles.wrapper}>
+                    <h2 className={styles.title}>CART TOTAL</h2>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Subtotal:</b>$79.60
+                    </div>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Discount:</b>$0.00
+                    </div>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Total:</b>$79.60
+                    </div>
+                    <button className={styles.button}>CHECKOUT NOW!</button>
+                </div>
+            </div>
         </div>
     )
 }
