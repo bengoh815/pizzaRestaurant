d2965902e121849f5425e0f5d5620335b8a09673
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2023-03-01 23:35:18 -0600

    Current state
    Need to get images

---
+++ pizzarestaurant/pages/orders/[id].jsx
@@ -0,0 +1,82 @@
+import styles from '../../styles/Order.module.css'
+import Image from 'next/image'
+
+export default function() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.left}>
+                <div className={styles.row}>
+                    <table className={styles.table}>
+                        <tbody>
+                            <tr className={styles.tr}>
+                                <th>Order ID</th>
+                                <th>Customer</th>
+                                <th>Address</th>
+                                <th>Total</th>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <span className={styles.id}>6354</span>
+                                </td>
+                                <td>
+                                    <span className={styles.name}>Joe Doe</span>
+                                </td>
+                                <td>
+                                    <span className={styles.address}>123 Flower Street, US</span>
+                                </td>
+                                <td>
+                                    <span className={styles.total}>$79.80</span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.status}>
+                        <Image src="" alt=""/>
+                        <span>Payment</span>
+                        <div className={styles.checkedIcon}>
+                            <Image src="" alt=""/>
+                        </div>
+                    </div>
+                    <div className={styles.status}>
+                        <Image src="" alt=""/>
+                        <span>Cooking</span>
+                        <div className={styles.checkedIcon}>
+                            <Image src="" alt=""/>
+                        </div>
+                    </div>
+                    <div className={styles.status}>
+                        <Image src="" alt=""/>
+                        <span>Delivering</span>
+                        <div className={styles.checkedIcon}>
+                            <Image src="" alt=""/>
+                        </div>
+                    </div>
+                    <div className={styles.status}>
+                        <Image src="" alt=""/>
+                        <span>Delivered</span>
+                        <div className={styles.checkedIcon}>
+                            <Image src="" alt=""/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.right}>
+            <div className={styles.wrapper}>
+                <h2 className={styles.title}>CART TOTAL</h2>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Subtotal:</b>$79.60
+                    </div>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Discount:</b>$0.00
+                    </div>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Total:</b>$79.60
+                    </div>
+                    <button disabled className={styles.button}>PAID</button>
+                </div>
+            </div>
+        </div>
+    );
+}
